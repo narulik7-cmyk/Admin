355e0ec485aef7fbebfc89ba1323082ba090e8a7
narulik7-cmyk <narulik7@gmail.com>
2026-08-30 10:24:27 +0200

    Add test/widget_test.dart

---
+++ test/widget_test.dart
@@ -0,0 +1,20 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:foodbite8/main.dart';
+
+void main() {
+  testWidgets('shows the FoodBite8 discovery page', (WidgetTester tester) async {
+    await tester.pumpWidget(const FoodBite8App());
+
+    expect(find.text('FoodBite8'), findsOneWidget);
+    expect(find.text('Good food is happening.'), findsOneWidget);
+  });
+}
+
